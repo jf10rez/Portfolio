ab997cbaf0d73b1d1b06242af012bcac6c103c53
Johavill <57880508+Johavill@users.noreply.github.com>
2022-06-06 16:55:42 -0500

    se sube el parallax

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,22 +1,43 @@
 import React from "react";
+import { Parallax, Background } from 'react-parallax';
+import img1 from '../../imgs/para-que-sirve-javascript.jpg';
+import img2 from '../../imgs/image2.jpg';
+import img3 from '../../imgs/foto.jpg';
 
 import "./about.css";
 
 export const About = () => {
+  const styles = {
+    fontFamily: "sans-serif",
+    textAlign: "center"
+  };
+  const insideStyles = {
+    background: "white",
+    padding: 20,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)"
+  };
   return (
-    <section id="section-about" className="contain-about">
-      <div className="row">
-        <div className="col-md-6">
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries.
-          </p>
-        </div>
-        <div className="col-md-6">
-
+    <section id="section-about">
+      <div className="container-fluid">
+        <div className="row">
+          <Parallax bgImage={img1} strength={500}>
+            <div style={{ height: "100vh" }}>
+              <div style={insideStyles}>HTML inside the parallax</div>
+            </div>
+          </Parallax>
+          <Parallax bgImage={img2} strength={500}>
+            <div style={{ height: "100vh" }}>
+              <div style={insideStyles}>HTML inside the parallax</div>
+            </div>
+          </Parallax>
+          <Parallax bgImage={img3} strength={500}>
+            <div style={{ height: "100vh" }}>
+              <div style={insideStyles}>HTML inside the parallax</div>
+            </div>
+          </Parallax>
         </div>
       </div>
     </section>
